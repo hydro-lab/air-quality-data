7bf83c37b0ecd2f3a1d577a21898315468d55d1d
David Kahler <dmkahler@users.noreply.github.com>
2020-04-07 15:28:29 -0400

    Add files via upload
    Added so2 (sulfur dioxide) into automatically pulled data options, added xlim argument to plot to zoom in on various times.

--- data_pull.r
+++ data_pull.r
@@ -9,9 +9,9 @@ library(dplyr)
 x = read.csv("aq31mar2020.csv")
 # Subset data frame for visualization.  Guidance from: https://www.r-bloggers.com/5-ways-to-subset-a-data-frame-in-r/
 # SELECT SITE HERE !!!!!!!!!!!!!!!!!!!!!!!!!
-sitename <- "North Braddock"
-y <- x[which(x$site == sitename), names(x) %in% c("datetime", "stat", "pm10","pm10_fl", "pm10b", "pm25", "pm25.2.", "pm25_fl", "pm25b", "pm25t")]
-z <- y[which(y$stat == "Avg"), names(y) %in% c("datetime", "pm10","pm10_fl", "pm10b", "pm25", "pm25.2.", "pm25_fl", "pm25b", "pm25t")]
+sitename <- "Clairton"
+y <- x[which(x$site == sitename), names(x) %in% c("datetime", "stat", "pm10", "pm10b", "pm25", "pm25.2.", "pm25b", "pm25t", "so2")]
+z <- y[which(y$stat == "Avg"), names(y) %in% c("datetime", "pm10","pm10b", "pm25", "pm25.2.", "pm25b", "pm25t", "so2")]
 
 day <- array("", dim = c(nrow(z),1))
 mes <- array(0, dim = c(nrow(z),1))
@@ -21,10 +21,10 @@ for (i in 1:(nrow(z))) {
   q <- tolower(p[[1]])
   day[i,1] <- q[1]
   # Insert data next to date.  SELECT MEASUREMENT HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  mes[i,1] <- c(z$pm10b[i])
+  mes[i,1] <- c(z$so2[i])
 }
 
 quality <- data.frame(as.Date(day), mes)
 sorted_quality <- quality[order(day),]
 
-plot(sorted_quality, type = "l", main = paste(sitename, "Air Quality", delim = " "), xlab = "date", ylab = "particulate matter, PM10b")
+plot(sorted_quality, type = "l", xlim = as.Date(c("2019-01-01", "2020-01-01")), main = paste(sitename, "Air Quality", delim = " "), xlab = "date (Jan 2019 to Jan 2020)", ylab = "sulfur dioxide (ppm)")
