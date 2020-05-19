17321907e625db3b9d71de92c7c41730a9ef6b94
David Kahler <dmkahler@users.noreply.github.com>
2020-05-19 16:41:16 -0400

    Add files via upload
    Added date parsing and statistics calculation.

--- data_pull.r
+++ data_pull.r
@@ -6,14 +6,17 @@ setwd("/Users/davidkahler/Documents/R/airquality/")
 # install.packages("dplyr")
 library(dplyr)
 library(latex2exp) # for using TeX commands in plotting, https://cran.r-project.org/web/packages/latex2exp/vignettes/using-latex2exp.html 
+library(e1071) # includes skewness and kurtosis functions
 
 # Locations of sites:
 # https://data.wprdc.org/dataset/c7b3266c-adc6-41c0-b19a-8d4353bfcdaf/resource/7f7072ce-7c19-4813-a45c-6135cf4505bb/download/aqsensors.geojson
 
-x = read.csv("aq30apr2020.csv")
+x = read.csv("aq_jan-apr.csv")
 # Subset data frame for visualization.  Guidance from: https://www.r-bloggers.com/5-ways-to-subset-a-data-frame-in-r/
 # SELECT SITE HERE !!!!!!!!!!!!!!!!!!!!!!!!!
-sitename <- "Harrison Township"
+# Solar radiation is at Lawrenceville
+# NO2 from Harrison Township and Parkway East
+sitename <- "Parkway East"
 y <- x[which(x$site == sitename), names(x) %in% c("datetime", "stat", "pm10", "pm10b", "pm25", "pm25.2.", "pm25b", "pm25t", "so2", "no2")]
 z <- y[which(y$stat == "Avg"), names(y) %in% c("datetime", "pm10","pm10b", "pm25", "pm25.2.", "pm25b", "pm25t", "so2", "no2")]
 # Pull solar radiation
@@ -21,20 +24,38 @@ y <- x[which(x$site == "Lawrenceville"), names(x) %in% c("datetime", "stat", "so
 z <- y[which(y$stat == "Avg"), names(y) %in% c("datetime", "solarrad")]
 
 day <- array("", dim = c(nrow(z),1))
+yea <- array("", dim = c(nrow(z),1))
+mon <- array("", dim = c(nrow(z),1))
+dom <- array("", dim = c(nrow(z),1)) # day of month
 mes <- array(0, dim = c(nrow(z),1))
 # Parse the date and time variables.  Guidance from: https://www.dummies.com/programming/r/how-to-split-strings-in-r/ and https://www.dummies.com/programming/r/how-to-convert-a-factor-in-r/
 for (i in 1:(nrow(z))) {
   p <- strsplit(as.character(z$datetime[i]), "T")
   q <- tolower(p[[1]]) # extracts the character vector from the list
   day[i,1] <- q[1] # stores the date in "day"
+  tmp1 <- as.Date(q[1])
+  tmp2 <- as.Date(tmp1, '%Y/%m/%d')
+  yea[i] <- format(tmp2, '%Y')
+  mon[i] <- format(tmp2, '%m')
+  dom[i] <- format(tmp2, '%d')
   # Insert data next to date.  SELECT MEASUREMENT HERE!!!!!!!!!!!!!!!!!!!!!!!!!!!
   mes[i,1] <- c(z$no2[i])
 }
 
-quality <- data.frame(as.Date(day), mes)
+quality <- data.frame(as.Date(day), yea, mon, dom, mes)
 sorted_quality <- quality[order(day),]
 
-plot(sorted_quality, type = "l", xlim = as.Date(c("2020-01-01", "2020-04-30")), main = paste(sitename, "Air Quality", delim = " "), xlab = "date", ylab = TeX('$Solar Radiation (W/m^{2})$'))
-plot(sorted_quality, type = "l", xlim = as.Date(c("2020-01-01", "2020-04-30")), main = paste(sitename, "Air Quality", delim = " "), xlab = "date (Jan 2020 to Apr 2020)", ylab = TeX('$NO_{2} (ppm)$'))
+plot(sorted_quality, type = "l", xlim = as.Date(c("2020-01-01", "2020-05-15")), main = paste(sitename, "Air Quality", delim = " "), xlab = "date", ylab = TeX('$Solar Radiation (W/m^{2})$'))
+plot(sorted_quality, type = "l", xlim = as.Date(c("2020-01-01", "2020-05-15")), main = paste(sitename, "Air Quality", delim = " "), xlab = "date", ylab = TeX('$NO_{2} (ppm)$'))
 
-     
+cor(sol_rad$mes, no2$mes, method = "pearson", use = "pairwise.complete.obs")
+
+no2 <- sorted_quality
+# hist(no2$mes)
+mean(no2$mes, na.rm = TRUE)
+sd(no2$mes, na.rm = TRUE)
+skewness(no2$mes, na.rm = TRUE)
+kurtosis(no2$mes, na.rm = TRUE)
+log_no2 = log(no2$mes) # no2 data appear to be log-normal, log defaults to natural log
+# hist(log_no2)
+
