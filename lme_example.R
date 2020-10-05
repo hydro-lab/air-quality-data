4fd894cfa77e3ef1cf89b67c690bd1f21693fc67
davidkahler <david.kahler@gmail.com>
2020-10-05 14:15:50 -0400

    Following https://www.apps.stat.vt.edu/kim/st5514/lab/lab_lme.html

---
+++ lme_example.R
@@ -0,0 +1,34 @@
+library(nlme)
+names(Orthodont)
+# [1] "distance" "age"      "Subject"  "Sex"
+levels(Orthodont$Sex)
+# [1] "Male"   "Female"
+OrthoFem <- Orthodont[Orthodont$Sex=="Female",]
+
+fmlOrthF <- lme(distance~age, data=OrthoFem, random = ~1|Subject)
+summary(fmlOrthF)
+# Linear mixed-effects model fit by REML
+# Data: OrthoFem 
+# AIC     BIC    logLik
+# 149.2183 156.169 -70.60916
+# 
+# Random effects:
+#       Formula: ~1 | Subject
+# (Intercept)  Residual
+# StdDev:     2.06847 0.7800331
+# 
+# Fixed effects: distance ~ age 
+# Value Std.Error DF   t-value p-value
+# (Intercept) 17.372727 0.8587419 32 20.230440       0
+# age          0.479545 0.0525898 32  9.118598       0
+# Correlation: 
+#       (Intr)
+# age -0.674
+# 
+# Standardized Within-Group Residuals:
+#       Min         Q1        Med         Q3        Max 
+# -2.2736479 -0.7090164  0.1728237  0.4122128  1.6325181 
+# 
+# Number of Observations: 44
+# Number of Groups: 11 
+
